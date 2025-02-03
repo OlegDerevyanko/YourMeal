48bffe2e307a0ce6743b155b986813d7a81ffa77
OlegDerevyanko <106957951+OlegDerevyanko@users.noreply.github.com>
2025-02-03 20:10:34 +0200

    up

---
+++ modal.js
@@ -0,0 +1,35 @@
+const buttonElem = document.querySelector('.basket__btn');
+const modalElem = document.querySelector('.basket__modal');
+
+modalElem.style.cssText = `
+    display: flex;
+    flex-direction: column;
+    visibility: hidden;
+    opacity: 0;
+    transition: opacity 1000ms ease-in-out;
+`;
+
+const closeModal = event => {
+    const target = event.target;
+
+    if (target === modalElem ||        
+        target.closest('.modal__end-close') ||
+        event.code === 'Escape') {
+        modalElem.style.opacity = 0;  
+
+        setTimeout(() => {
+            modalElem.style.visibility = 'hidden'; 
+        }, 1000);
+
+        window.removeEventListener('keydown', closeModal);
+    }
+}
+
+const openModal = () => {
+    modalElem.style.visibility = 'visible';
+    modalElem.style.opacity = 1;
+    window.addEventListener('keydown', closeModal)
+};
+
+buttonElem.addEventListener('click', openModal);
+modalElem.addEventListener('click', closeModal);
